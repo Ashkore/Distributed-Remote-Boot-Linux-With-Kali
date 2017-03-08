73a5ade8148bfb24a46e4e7fa932796212307e56
Ashkore <Saboyer@ollusa.edu>
2017-03-08 10:02:31 -0600

    Fix?

--- drbl.sh
+++ drbl.sh
@@ -7,23 +7,28 @@ NC='\033[0m'
 
 #Check if any Parameters are NULL
 if [ -z "$1" ]; then
-    echo -e "${RED}ERROR: Missing Parameter 1\nNIC not defined."
+    echo -e "${RED}ERROR: Missing Parameter 1\nDRBL NIC not defined."
     exit 0;
-elif [ -z "$2" ]; then
-    echo -e "${RED}ERROR: Missing Parameter 2\nIP Address of DRBL ENV NIC not defined."
+if [ -z "$2" ]; then
+    echo -e "${RED}ERROR: Missing Parameter 1\nOTHER NIC not defined."
     exit 0;
 elif [ -z "$3" ]; then
+    echo -e "${RED}ERROR: Missing Parameter 2\nIP Address of DRBL ENV NIC not defined."
+    exit 0;
+elif [ -z "$4" ]; then
     echo -e "${RED}ERROR: Missing Parameter 3\nGateway of DRBL ENV not defined."
     exit 0;
 fi
 
 #Check if IP address is an ipv4 address, if so, set the network and broadcast variables
-IFS=. read ip1 ip2 ip3 ip4 <<< $2
+IFS=. read ip1 ip2 ip3 ip4 <<< $3
 if [ -z $ip1 ] || [ -z $ip2 ] || [ -z $ip3 ] || [ -z $ip4 ]; then
     echo -e "${RED}ERROR: Incorrect Parameter 2\nIP address does not have 4 octets."
     exit 0;
 fi
 
+DRBLNic=$1
+OtherNic=$2
 first3=$ip1"."$ip2"."$ip3
 network=$first3".0"
 broadcast=$first3".255"
@@ -32,13 +37,13 @@ broadcast=$first3".255"
 echo -e "${LGreen}Starting: Setting up the /etc/network/interfaces File."
 echo "#THE DRBL ENVIRONMENT" >> /etc/network/interfaces
 echo "#This interface is for the NIC pointing to the DRBL network." >> /etc/network/interfaces
-echo "allow-hotplug $1" >> /etc/network/interfaces
-echo "iface $1 inet static" >> /etc/network/interfaces
-echo "address $2" >> /etc/network/interfaces
+echo "allow-hotplug $DRBLNic" >> /etc/network/interfaces
+echo "iface $DRBLNic inet static" >> /etc/network/interfaces
+echo "address $3" >> /etc/network/interfaces
 echo "netmask 255.255.225.0" >> /etc/network/interfaces
 echo "network $network" >> /etc/network/interfaces
 echo "broadcast $broadcast" >> /etc/network/interfaces
-echo "gateway $3" >> /etc/network/interfaces
+echo "gateway $4" >> /etc/network/interfaces
 echo -e "${Green}Finished: Setting up the /etc/network/interfaces File."
 #End of Networking setup
 
@@ -72,12 +77,36 @@ echo -e "${Green}Finished: Installing DRBL"
 
 
 exit 0;
+
 #WORK IN PROGRESS
 echo -e "${LGreen}Starting: DRBL Configuration"
+
+echo -e "${LGreen}Starting: DRBL package Installs"
+apt-get install drbl-chntpw freedos partclone clonezilla mkpxeinitrd-net -y --allow-unauthenticated
 printf 'N\n
         N\n
         Y\n
-        1\n
+        1\n' | drblsrv -i
+echo -e "${LGreen}Finished: DRBL package Installs"
+
+echo -e "${LGreen}Starting: DRBL Client Setup"
+#Please Enter DNS Domain [drbl.org]
+#Please enter NIS/YP domain name [penguinzilla]
+#Please enter the client hostname prefix [DRBL-Server-]
+#Public IP of this server [eth0]
+printf '\n
+        \n
+        \n
+        $OtherNIC\n
+
+        ' | drblpush -i
+
+
+
+
+echo -e "${LGreen}Finished: DRBL Client Setup"
+
+echo -e "${LGreen}Finished: DRBL Configuration"
 
 
 
