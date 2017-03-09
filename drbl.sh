c9ee99a9fb9ded118ad2910c8a7db8692ec51fbf
Ashkore <Saboyer@ollusa.edu>
2017-03-08 21:54:45 -0600

    Networking Fix stuff

--- drbl.sh
+++ drbl.sh
@@ -1,59 +1,11 @@
 #!/usr/bin/env bash
+#EXAMPLE:
+#./drbl.sh
 #Color Variables
 Green='\033[0;32m'
 LGreen='\033[1;32m'
 RED='\033[0;31m'
 NC='\033[0m'
-
-#Check if any Parameters are NULL
-if [ -z "$1" ]; then
-    echo -e "${RED}ERROR: Missing Parameter 1\nDRBL NIC not defined."
-    exit 0;
-fi
-if [ -z "$2" ]; then
-    echo -e "${RED}ERROR: Missing Parameter 1\nOTHER NIC not defined."
-    exit 0;
-fi
-if [ -z "$3" ]; then
-    echo -e "${RED}ERROR: Missing Parameter 2\nIP Address of DRBL ENV NIC not defined."
-    exit 0;
-fi
-if [ -z "$4" ]; then
-    echo -e "${RED}ERROR: Missing Parameter 3\nGateway of DRBL ENV not defined."
-    exit 0;
-fi
-
-#Check if IP address is an ipv4 address, if so, set the network and broadcast variables
-IFS=. read ip1 ip2 ip3 ip4 <<< $3
-if [ -z $ip1 ] || [ -z $ip2 ] || [ -z $ip3 ] || [ -z $ip4 ]; then
-    echo -e "${RED}ERROR: Incorrect Parameter 2\nIP address does not have 4 octets."
-    exit 0;
-fi
-
-DRBLNic=$1
-OtherNic=$2
-first3=$ip1"."$ip2"."$ip3
-network=$first3".0"
-broadcast=$first3".255"
-
-#Start of Networking setup
-echo -e "${LGreen}Starting: Setting up the /etc/network/interfaces File."
-echo "#THE DRBL ENVIRONMENT" >> /etc/network/interfaces
-echo "#This interface is for the NIC pointing to the DRBL network." >> /etc/network/interfaces
-echo "allow-hotplug $DRBLNic" >> /etc/network/interfaces
-echo "iface $DRBLNic inet static" >> /etc/network/interfaces
-echo "address $3" >> /etc/network/interfaces
-echo "netmask 255.255.225.0" >> /etc/network/interfaces
-echo "network $network" >> /etc/network/interfaces
-echo "broadcast $broadcast" >> /etc/network/interfaces
-echo "gateway $4" >> /etc/network/interfaces
-ifup $DRBLNic
-echo -e "${Green}Finished: Setting up the /etc/network/interfaces File."
-#End of Networking setup
-
-echo -e "${LGreen}Starting: Restarting networking service."
-service networking restart
-echo -e "${Green}Finished: Restarting networking service."
 
 #Start of Repository setup
 echo -e "${LGreen}Starting: Setting up /etc/apt/sources.list File"
@@ -96,7 +48,7 @@ echo -e "${LGreen}Starting: DRBL Client Setup"
 #Please enter the client hostname prefix [DRBL-Server-]
 #Public IP of this server [eth0]
 echo '\n\n\n$OtherNIC\n'
-printf '\n\n\n$OtherNIC\n' | drblpush -i
+printf '\n\n\n  \n' | drblpush -i
 
 
 
--- index.md
+++ index.md
@@ -32,7 +32,44 @@ I take a snapshot here since it is a virtual machine. [Fresh Install]
 root@kali:~# apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
 ```
 I take a snapshot here since it is a virutal machine. [Updated]
-### 3. Script to install packages
+### 3. Setup Network
+```
+root@kali:~# ifconfig
+
+        eth0:   .....
+            inet 192.168.116.129 netmask 255.255.255.0 broadcast 192.168.116.255
+                .....
+        eth1:
+                .....
+                
+root@kali:~# nano /etc/network/interfaces
+        # The lookpback network interface
+        auto lo
+        iface lo inet loopback
+        
+        # The NIC point to the Internet
+        auto eth0
+        iface eth0 inet dhcp
+ 
+        # The DRBL NIC
+        auto eth1
+        allow-hotplug eth1
+        iface eth1 inet static
+            address 10.0.0.2
+            netmask 255.255.255.0
+            gateway 10.0.0.1
+    
+root@kali:~# ifup eth1
+root@kali:~# ifconfig
+        eth0:   .....
+            inet 192.168.116.129 netmask 255.255.255.0 broadcast 192.168.116.255
+                .....
+        eth1:   .....
+            inet 10.0.0.2 netmask 255.255.255.0 broadcast 10.0.0.255
+                .....
+
+```
+### 4. Script to install packages
 ```
 Download the Script
 root@kali:~# wget https://ashkore.github.io/Distributed-Remote-Boot-Linux-With-Kali/drbl.sh
