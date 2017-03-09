1ed8a75a06c9c3da030d81a3bae1d1381858f3f8
Ashkore <Saboyer@ollusa.edu>
2017-03-08 22:37:10 -0600

    Testing

---
+++ Test.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+printf '\n\nKali-\n\n\n\n10\n20\n\n\n\n2\nN\n\n\n\n\n\n\n\n\n\n\n\n\n\n' | drblpush -i
--- drbl.sh
+++ drbl.sh
@@ -33,25 +33,45 @@ echo -e "${Green}Finished: Installing DRBL"
 echo -e "${LGreen}Starting: DRBL Configuration"
 
 echo -e "${LGreen}Starting: DRBL package Installs"
+echo -e "${NC}"
 apt-get install drbl-chntpw freedos partclone clonezilla mkpxeinitrd-net -y --allow-unauthenticated
 printf 'N\nN\nY\n1\n' | drblsrv -i
-echo -e "${LGreen}Finished: DRBL package Installs"
+echo -e "${Green}Finished: DRBL package Installs"
 
 
 
 exit 0;
 echo -e "${LGreen}Starting: DRBL Client Setup"
+echo -e "${NC}"
 #Please Enter DNS Domain [drbl.org]
 #Please enter NIS/YP domain name [penguinzilla]
 #Please enter the client hostname prefix [DRBL-Server-]
 #Public IP of this server [eth0]
-echo '\n\n\n$OtherNIC\n'
-printf '\n\n\n  \n' | drblpush -i
+#Collect MACaddresses? [N]
+#DCHP in DRBL NIC offer same IP to each mac everytime [n]
+#Inital number to use for ip [1] 10
+#How many DRBL clients? [12] 20
+#Correct IPS? [y]
+#Press enter to continue
+#Which DRBL mode? [0]
+#Which clonezilla[0] 2
+#Want to use the HDD Swap partition as swapfile [Y] N
+#Which mode do you want the clients to use after they boot? [1]
+#which mode do you want went client boots [0]
+#Differnt root password for clients vs server? [N]
+#PXE password [N]
+#Boot Prompts for clients? [Y]
+#How many 1/10 sec is the boot promt timeout for clients? [70]
+#Graphic background for PXE menu? [Y]
+#Let USb stuff work [y]
+#public IP for cleints? [N]
+#Terminal mode? [N]
+#Let DRBL server as a NAT server? [Y]
+#press enter to continue
+#Continue[Y]
 
+printf '\n\nKali-\n\n\n\n10\n20\n\n\n\n2\nN\n\n\n\n\n\n\n\n\n\n\n\n\n\n' | drblpush -i
+echo -e "${Green}Finished: DRBL Client Setup"
 
-
-
-echo -e "${LGreen}Finished: DRBL Client Setup"
-
-echo -e "${LGreen}Finished: DRBL Configuration"
+echo -e "${Green}Finished: DRBL Configuration"
 #End of DRBL Configuration
